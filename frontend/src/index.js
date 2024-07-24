e1ae78ebf1ea944bb18b7f6371c677d4c81d2757
Polina Zimenkova <polina.kren8952@mail.ru>
2024-07-24 22:59:33 +0200

    task-11

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/style.scss';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 
+import './assets/style.scss';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rollbarConfig from './rollbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider config={rollbarConfig}>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </Provider>,
 );
 
 reportWebVitals();
